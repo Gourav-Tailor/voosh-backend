f8288ea6e7c3f4ee7d34b7675ab148e3d64ec145
Gourav Tailor <gauravmanishtailor0@gmail.com>
2023-06-21 14:57:15 +0530

    MONGODB string attached without env

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_KEY);
+mongoose.connect("mongodb+srv://gouravtailor:gouravtailor@merntest.cbyv9p4.mongodb.net/Voosh?retryWrites=true&w=majority");
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
