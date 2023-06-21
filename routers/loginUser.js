7121842d6d901dfefc3b7a091e8f16a565338b22
Gourav Tailor <gauravmanishtailor0@gmail.com>
2023-06-21 20:54:44 +0530

    Password Match Issue

--- routers/loginUser.js
+++ routers/loginUser.js
@@ -8,27 +8,29 @@ require('dotenv').config();
 router.post('/login-user', (req,res)=>{
     const  { phone_number, password, login_by } = req.body;
     
-    bcrypt.genSalt(10, (err, Salt) => {
-        bcrypt.hash(password, Salt, (err, hash) => {
-            if (err) {
+        User.findOne({phoneNumber: phone_number})
+            .then(data => {
+                    if(data){
+                        bcrypt.compare(password, data.password,
+                            function (err, isMatch) {
+                                if(isMatch){
+                                    const token = jwt.sign({ userId: data.userId }, process.env.SECRET_KEY, { expiresIn: '1h' });
+                                    res.set('authorization', token);
+                                    res.status(200).json({token});
+                                }
+                                else{
+                                    res.status(500).json({ message: err });
+                                }
+                                
+                            });
+                    }
+                    else {
+                        res.status(500).json({ message: "Invaild Credentials" });
+                    }
+            })
+            .catch(err => {
                 res.status(500).json({ message: err });
-            }
-            User.findOne({phoneNumber: phone_number,password: hash})
-                .then(data => {
-                        if(data){
-                            const token = jwt.sign({ userId: data.userId }, process.env.SECRET_KEY, { expiresIn: '1h' });
-                            res.set('authorization', token);
-                            res.status(200).json({token});
-                        }
-                        else {
-                            res.status(500).json({ message: "Invaild Credentials" });
-                        }
-                })
-                .catch(err => {
-                    res.status(500).json({ message: err });
-                });
-            });
-    });
+        });
 });
 
 module.exports = router;
